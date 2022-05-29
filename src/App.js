bd8986b8de37bc7220803c3d1b524fc86243c97a
gyanAsh <gyanash36@protonmail.com>
2022-05-29 09:28:31 +0530

    Added Search Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import './App.css';
 import Navbar from './Components/Layout/Navbar'
 import Users from './Components/Users/Users'
+import Search from './Components/Users/Search'
 import axios from 'axios';
 class App extends Component {
   state = {
@@ -9,10 +10,12 @@ class App extends Component {
     loading: false
   }
 
-  async componentDidMount() {
+
+
+  searchUsers = async (text) => {
     this.setState({ loading: true });
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({ users: res.data, loading: false });
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ users: res.data.items, loading: false });
   }
 
   render(){
@@ -20,6 +23,7 @@ class App extends Component {
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
         <div className="container">
+          <Search searchUsers={this.searchUsers}/>
           <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
---
+++ src/Components/Users/Search.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+export class Search extends Component {
+
+    state = {
+        text: ''
+        
+    };
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired
+    }
+
+    onChange = (e) => {
+        this.setState({ text: e.target.value });
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.searchUsers(this.state.text);
+        this.setState({ text: "" });
+    }
+
+  render() {
+    return (
+        <div>
+            <form className="form" onSubmit={this.onSubmit}>
+                <input type="text" name="text" onChange={this.onChange} placeholder="Search Users..." value={this.state.text} />
+                <input type="submit" value="Search" className="btn btn-dark btn-block"/>
+            </form>
+      </div>
+    )
+  }
+}
+
+export default Search;
